ff5b7e963d2a5413405184533f1c2a98e92e6e08
john <johnripper5150@gmail.com>
2019-11-23 20:48:50 +0000

    urlbuilder

--- lib/api.py
+++ lib/api.py
@@ -6,10 +6,25 @@ from lib.exceptions import HttpStatus
 from lib.objects import SelfBot
 
 
+class UrlBuilder:
+    LOGIN = "https://jumpin.chat/login"
+    LOGOUT = "https://jumpin.chat/logout"
+    SESSION = "https://jumpin.chat/api/user/session"
+    _sio = "https://jumpin.chat/socket.io/?token={token}&EIO=3&transport=polling&t=Muk-CB0"
+    _wss = "wss://jumpin.chat/socket.io/?token={token}&EIO=3&transport=websocket&sid={io}"
+
+    def sio(self, token):
+        return self._sio.format(token=token)
+
+    def wss(self, token, io):
+        return self._wss.format(token=token, io=io)
+
+
 class Api:
     def __init__(self):
         self.client = ClientSession()
         self._session = None
+        self.urls = UrlBuilder()
 
     async def post(self, url: str = None, data: dict = None):
         result = await self.client.post(url=url, data=data)
@@ -19,32 +34,34 @@ class Api:
             return result
 
     async def get(self, url: str = None):
-        result = await self.client.post(url=url)
+        result = await self.client.get(url=url)
         if result.status != 200:
             raise HttpStatus(code=result.status)
         else:
             return result
 
-    async def session(self):
+    @property
+    def session(self):
         if self._session:
-            # todo get guest session by default.
-            return None
-        return self._session
+            return self._session
+        # todo return a guest session  by default if the session object is None.
+
+    async def logout(self):
+        await self.post(url=self.urls.LOGOUT)
 
     async def login(self, username: str, password: str):
-        await self.post(url='https://jumpin.chat/login',
+        await self.post(url=self.urls.LOGIN,
                         data={"action": "login",
                               "username": username,
                               "password": password})
         # todo check if successful or not. consider logging in as guest
-        resp = await self.post('https://jumpin.chat/api/user/session')
-        self.session = SelfBot(**json.loads(await resp.text()))
-        r = await self.get(f"https://jumpin.chat/socket.io/?token={self.session.token}&EIO=3&transport=polling&t=Muk-CB0")
+        resp = await self.post(self.urls.SESSION)
+        self._session = SelfBot(**json.loads(await resp.text()))
+        r = await self.get(self.urls.sio(token=self._session))
         pattern = r"(?<=\"sid\":\")(.*?)(?=\",)"
         io = regex.search(pattern, await r.text())
         # # hmm cookies instead?
         print(self.client.cookie_jar.__dict__)
         # for cookie  in self.session.cookie_jar:
         #     print(cookie)
-        url = f"wss://jumpin.chat/socket.io/?token={self.session.token}&EIO=3&transport=websocket&sid={io[0]}"
-        return url
+        return self.urls.wss(token=self._session.token, io=io[0])
