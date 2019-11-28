a5d9641570b3eb40f7f61cce58d40b9f04e2946d
john <johnripper5150@gmail.com>
2019-11-28 20:25:06 +0000

    help/readme template

--- modules/debug.py
+++ modules/debug.py
@@ -8,9 +8,30 @@ from lib.styling import Colors, Styles, encodetxt
 
 class Debug(Cog):
 
-    #####
-    # Events
-    #####
+    async def get_template(self):
+
+        f = open('./data/help_template.md', 'r')
+        text = f.read()
+        command_data = ""
+
+        for cog in self.bot.cm.cogs.values():
+            for command in cog.commands:
+                print(command)
+                command_data += (f"{getattr(command, '__command_name__')}: {getattr(command, '__description__')}\n")
+                print(command_data)
+        text = text.format(commands=command_data)
+        f.close()
+        return text
+
+    @makeCommand(name="t", description="test")
+    async def generate_readme(self, c: Command):
+        generated = await self.get_template()
+        f = open('./README.md', 'w')
+        f.write(generated)
+        f.flush()
+        f.close()
+
+
     @makeCommand(name="userlist", description="test")
     async def vagina(self, c: Command):
         await self.send_message(json.dumps(await self.bot.userlist))
@@ -30,6 +51,10 @@ class Debug(Cog):
         else:
             formated = encodetxt(c.message, Styles.script)
         await self.send_message(formated)
+
+    #####
+    # Events
+    #####
 
     @event(event="room::message")
     async def message(self, message: Message):
