67fdcdf8b11aca4940e4e7929f4a4a0ab2804feb
john <johnripper5150@gmail.com>
2019-11-28 14:06:09 +0000

    added the message object to Command, can now retrieve info like messagetime and sender.

--- lib/command.py
+++ lib/command.py
@@ -5,13 +5,10 @@ from lib.objects import Message
 
 def makeCommand(name: str, description: str, **attrs):
     def wrap(f):
-        if isinstance(f, Command):
-            raise TypeError('Callback is already a command.')
         f.__command__ = True
         f.__command_name__ = name
-        f.description = description
+        f.__description__ = description
         return f
-
     return wrap
 
 
@@ -20,8 +17,10 @@ command_pattern = "{}(\w+)(\\b.*)"
 
 class Command:
     def __init__(self, prefix: str, data: Message):
+
         self.prefix = prefix
         self.name, self.message = re.search(
             command_pattern.format(self.prefix),
             data.message).groups()
         self.message = self.message.lstrip()
+        self.data = data
