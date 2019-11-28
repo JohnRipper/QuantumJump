b4c6360c9c450d9ac084c772335602095a964922
john <johnripper5150@gmail.com>
2019-11-28 14:08:54 +0000

    removed .is_running added .state

--- blumpkin.py
+++ blumpkin.py
@@ -1,27 +1,29 @@
 import asyncio
 import json
 import time
+from enum import Enum
 
 import websockets
 
 from lib.api import Api
 from lib.cog import CogManager
 from lib.command import Command
-from lib.objects import Message, User, UserList
+from lib.objects import Message, User, UserList, BotState
 
 
 class QuantumJumpBot:
     def __init__(self, settings):
         self._ws = None
-        self.is_running = False
+        self.state = BotState(BotState.INITIALIZED)
         self.start_time = time.time()
         self.settings = settings
         self.api = Api()
         self.cm = CogManager()
+        self.room = self.settings.Bot.roomname
 
     @property
     async def userlist(self) -> [User]:
-        data = await self.api.getroominfo(room=str(self.settings.Bot.roomname))
+        data = await self.api.getroominfo(room=str(self.room))
         ul = UserList(**data)
         return ul.users
 
@@ -30,7 +32,8 @@ class QuantumJumpBot:
             data = "42{}".format(json.dumps(data))
             await self._ws.send(data)
         elif type(data) is str:
-            if not data.startswith("42") and not data == "2probe" and not data == "5" and not data == "2":
+            type_exceptions = ["2probe", "5", "2"]
+            if not data.startswith("42") and data not in type_exceptions:
                 data = f"42{data}"
         else:
             print("invalid data type for wsend")
@@ -42,7 +45,7 @@ class QuantumJumpBot:
         await self.connect()
 
     async def disconnect(self):
-        self.is_running = False
+        self.state = BotState.DISCONNECT
         await self._ws.close()
 
     async def connect(self):
@@ -54,7 +57,7 @@ class QuantumJumpBot:
                 timeout=600,
                 origin="https://jumpin.chat") as self._ws:
             print("Socket started")
-            self.is_running = True
+            self.state = BotState.RUNNING
             await self.wsend("2probe")
             async for message in self._ws:
                 await self._recv(message=message)
@@ -67,12 +70,12 @@ class QuantumJumpBot:
             await self.wsend("5")
             await self.wsend(
                 ["room::join", {
-                    "room": self.settings.Bot.roomname
+                    "room": self.room
                 }])
             asyncio.create_task(self.pacemaker())
             return
         data = json.loads(message[2:])
-        await self.cm.do_event(data=data)
+        await self.cm.do_event(ree=data)
         if data[0] == "self::join":
             await self.wsend([
                 "room::handleChange", {
@@ -90,21 +93,16 @@ class QuantumJumpBot:
                 if c.name == "reload" or c.name == "load":
                     if m := self.cm.import_module(c.message):
                         self.cm.add_cog(m, c.message, self)
-
                     else:
-                        await self.wsend(Message.makeMsg(message=f"failed to load|reload {c.message}",
-                                                         room=self.settings.Bot.roomname))
+                        await self.wsend(Message.makeMsg(message=f"failed to load|reload {c.message}", room=self.room))
                 if c.name == "unload":
-                    if m := self.cm.unload(c.message):
-                        await self.wsend(Message.makeMsg(message=f"unloaded {c.message}",
-                                                         room=self.settings.Bot.roomname))
+                    if self.cm.unload(c.message):
+                        await self.wsend(Message.makeMsg(message=f"unloaded {c.message}", room=self.room))
                     else:
-                        await self.wsend(Message.makeMsg(message=f"Could not unload {c.message}",
-                                                         room=self.settings.Bot.roomname))
-
+                        await self.wsend(Message.makeMsg(message=f"Could not unload {c.message}", room=self.room))
 
     async def pacemaker(self):
-        if self.is_running:
+        if self.state == BotState.RUNNING:
             await asyncio.sleep(25)
             await self.wsend("2")
             asyncio.create_task(self.pacemaker())
@@ -112,21 +110,20 @@ class QuantumJumpBot:
     def process_input(self, loop):
         prefix = self.settings.Bot.prefix
         while True:
-            if self.is_running:
+            if self.state == BotState.RUNNING:
                 stdin = input()
                 if stdin.startswith(prefix):
                     asyncio.run_coroutine_threadsafe(
                         self._recv(message=Message(message=stdin).jumpson()), loop=loop)
                 else:
-                    asyncio.run_coroutine_threadsafe(self.wsend(Message.makeMsg(message=stdin,
-                                                                                room=self.settings.Bot.roomname)),
-                                                     loop=loop)
+                    asyncio.run_coroutine_threadsafe(self.wsend
+                                                     (Message.makeMsg(message=stdin, room=self.room)), loop=loop)
+            else:
+                if self.state == BotState.DISCONNECT or self.state == BotState.EXCEPTION:
+                    break
 
     async def process_message_queue(self):
-        if self.is_running:
+        if self.state is BotState.RUNNING:
             asyncio.run(asyncio.sleep(1))
             # await self.send_message()
             asyncio.create_task(self.process_message_queue())
-
-    async def GetClasses(self):
-        return [x for x in globals() if hasattr(globals()[str(x)], '__cog__')]
