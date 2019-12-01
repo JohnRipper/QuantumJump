a40ff7f8694bd6094be8088312cc0a10002d935f
Aida <Autotonic@users.noreply.github.com>
2019-12-01 08:24:48 -0500

    dont fix things while half asleep

--- modules/builtins.py
+++ modules/builtins.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from lib.cog import Cog
 from lib.command import Command, makeCommand
 from lib.styling import Styles
-from util import get_current_sha1  # , get_latest_sha1
+from util import get_current_sha1, get_latest_sha1
 
 
 class Builtins(Cog):
     @makeCommand(name="version", description="get the current version")
     async def version(self, c: Command):
         message = ":crystal_ball: currently using: *{}* | Latest is: *{}*".format(
-            get_current_sha1(), "N/A"
+            get_current_sha1(),
             await get_latest_sha1()
         )
         await self.send_message(message)
