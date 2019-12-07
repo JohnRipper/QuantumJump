e7a41cdc3d58c42650b302e3d78c452bc9eb6eb1
Aida <Autotonic@users.noreply.github.com>
2019-12-06 23:55:38 -0500

    learn2variable

--- modules/youtube.py
+++ modules/youtube.py
@@ -12,16 +12,17 @@ class Youtube(Cog):
     def __init__(self, bot):
         super().__init__(bot)
         self.headers = None
+        self.api_key = self.settings["api_key"]
 
-        if len(self.settings["api_key"]) == 0:
-            self.settings["api_key"] = "AIzaSyCPQe4gGZuyVQ78zdqf9O5iEyfVLPaRwZg"
+        if len(self.api_key) == 0:
+            self.api_key = "AIzaSyCPQe4gGZuyVQ78zdqf9O5iEyfVLPaRwZg"
             self.headers = {"referer": "https://tinychat.com"}
 
     async def ytsearch(self, query: str) -> dict:
         searchurl = "https://www.googleapis.com/youtube/v3/search?"\
             "part=snippet&type=video&q={query}&maxResults=1&"\
             "videoSyndicated=true&key={key}"
-        url = searchurl.format(query=query, key=self.settings["api_key"])
+        url = searchurl.format(query=query, key=self.api_key)
         async with aiohttp.ClientSession(headers=self.headers) as session:
             async with session.get(url) as response:
                 ytjson = await response.json()
@@ -31,14 +32,12 @@ class Youtube(Cog):
 
     @makeCommand(aliases=["yt"], description="<query | url> play youtube")
     async def playyt(self, c: Command):
-        print(c.message)
         if re.match("youtube(.be\/|.com\/watch\?)", c.message):
             ytid = re.search("(?:v=|\.be\/)(.{11})", c.message)[1]
             lazylook = await self.ytsearch(ytid)
             await self.play(video_id=ytid, title=lazylook["title"])
         else:
             ytinfo = await self.ytsearch(c.message)
-            print(ytinfo)
             await self.play(video_id=ytinfo["video_id"], title=ytinfo["title"])
 
     @makeCommand(aliases=["rm"], description="")
